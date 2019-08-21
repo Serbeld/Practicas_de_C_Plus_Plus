667c934a325332b4e0c552396a1ca717c110ca8a
Sergio Luis Beleño Díaz <38571812+Serbeld@users.noreply.github.com>
2019-08-21 00:43:21 -0500

    Hola mundo

---
+++ Hola_mundo.cpp
@@ -0,0 +1,17 @@
+// Primer Programa en c++
+
+# include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	cout <<"Hola mundo :D\n"<<endl;
+	cout <<"Hola"<<endl;
+	cout <<"Mundo\n";
+	cout <<"Dije!!"<<endl;
+	
+	return 0;
+}
+
+
