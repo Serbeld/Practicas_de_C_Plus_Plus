35fa676ae13aed3b761b2efc68cd0178dfb76074
Sergio Luis Beleño Díaz <38571812+Serbeld@users.noreply.github.com>
2019-08-21 21:50:05 -0500

    Tipos de datos

---
+++ TiposdeDatos.cpp
@@ -0,0 +1,23 @@
+// Tipos de datos básicos de C++
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+int entero = 14;
+float flotante = 12.45;
+double mayor = 16.321165;
+char letra = 'A';
+
+cout<<entero<<endl;		//Imprime el numero entero
+cout<<flotante<<endl;	//Imprime el numero flotante
+cout<<mayor<<endl;		//Imprime el numero double
+cout<<letra<<endl;		//Imprime la letra o caracter
+
+return 0;
+
+}
+
+
